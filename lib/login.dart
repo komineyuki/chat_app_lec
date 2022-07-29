0138ea1357492dcec1c2c95f9be1c2f4b41dbe9a
komineyuki <yukikomine1521@gmail.com>
2022-07-29 12:53:38 +0900

    w

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,6 @@
-import 'package:chat_app/chat.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'chat.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
